344070637e9afafad508ef986c269e40fe246c09
Sangjun Lee <85214907+sangjun-Leee@users.noreply.github.com>
2024-05-18 20:30:40 +0900

    Update graph_method.py

--- models/graph_method.py
+++ models/graph_method.py
@@ -6,6 +6,7 @@ from .common import MLP
 from .gcn import GCN, GCNII
 from .word_embedding import load_word_embeddings
 import scipy.sparse as sp
+from .compcos import compute_cosine_similarity
 
 
 def adj_to_edges(adj):
@@ -31,16 +32,48 @@ class GraphFull(nn.Module):
         self.args = args
         self.dset = dset
 
+        self.attrs = dset.attrs
+        self.objs = dset.objs
+
+        self.pairs = dset.pairs
+        self.pair2idx = dset.all_pair2idx
+
         self.val_forward = self.val_forward_dotpr
         self.train_forward = self.train_forward_normal
+
+        self.feasibility_adjacency = args.feasibility_adjacency
+        self.cosloss = args.cosine_classifier
+
+        self.known_pairs = dset.train_pairs
+        seen_pair_set = set(self.known_pairs)
+        mask = [1 if pair in seen_pair_set else 0 for pair in self.pairs]
+        self.seen_mask = torch.BoolTensor(mask).to(device) * 1.
+
+        self.feasibility_scores = {}
+        self.feasibility_margins = -(1 - self.seen_mask).float()
+        self.init_feasibility_scores()
+
+        self.epoch_max_margin = self.args.epoch_max_margin
+        self.scale = self.args.cosine_scale
+        self.cosine_margin_factor = -args.margin
+
+        # Intsantiate attribute-object relations, needed just to evaluate mined pairs
+        self.obj_by_attrs_train = {k: [] for k in self.attrs}
+        for (a, o) in self.known_pairs:
+            self.obj_by_attrs_train[a].append(o)
+
+        # Intanstiate attribute-object relations, needed just to evaluate mined pairs
+        self.attrs_by_obj_train = {k: [] for k in self.objs}
+        for (a, o) in self.known_pairs:
+            self.attrs_by_obj_train[o].append(a)
+
         # Image Embedder
-        self.num_attrs, self.num_objs, self.num_pairs = len(dset.attrs), len(dset.objs), len(dset.pairs)
-        self.pairs = dset.pairs
+        self.num_attrs, self.num_objs, self.num_pairs = len(dset.attrs), len(dset.objs), len(self.pairs)
 
         if self.args.train_only:
             train_idx = []
             for current in dset.train_pairs:
-                train_idx.append(dset.all_pair2idx[current]+self.num_attrs+self.num_objs)
+                train_idx.append(dset.all_pair2idx[current] + self.num_attrs + self.num_objs)
             self.train_idx = torch.LongTensor(train_idx).to(device)
 
         self.args.fc_emb = self.args.fc_emb.split(',')
@@ -50,8 +83,12 @@ class GraphFull(nn.Module):
             layers.append(a)
 
         if args.nlayers:
-            self.image_embedder = MLP(dset.feat_dim, args.emb_dim, num_layers= args.nlayers, dropout = self.args.dropout,
-                norm = self.args.norm, layers = layers, relu = True)
+            self.image_embedder = MLP(dset.feat_dim, args.emb_dim, num_layers=args.nlayers, dropout=self.args.dropout,
+                                      norm=self.args.norm, layers=layers, relu=True)
+            self.img2attr = MLP(dset.feat_dim, args.emb_dim, relu=args.relu, num_layers=args.nlayers, dropout=self.args.dropout,
+                                      norm=self.args.norm, layers=layers)
+            self.img2obj = MLP(dset.feat_dim, args.emb_dim, relu=args.relu, num_layers=args.nlayers, dropout=self.args.dropout,
+                                      norm=self.args.norm, layers=layers)
 
         all_words = list(self.dset.attrs) + list(self.dset.objs)
         self.displacement = len(all_words)
@@ -74,8 +111,19 @@ class GraphFull(nn.Module):
         if args.gcn_type == 'gcn':
             self.gcn = GCN(adj, self.embeddings.shape[1], args.emb_dim, hidden_layers)
         else:
-            self.gcn = GCNII(adj, self.embeddings.shape[1], args.emb_dim, args.hidden_dim, args.gcn_nlayers, lamda = 0.5, alpha = 0.1, variant = False)
+            self.gcn = GCNII(adj, self.embeddings.shape[1], args.emb_dim, args.hidden_dim, args.gcn_nlayers, lamda=0.5,
+                             alpha=0.1, variant=False)
 
+
+    def init_feasibility_scores(self):
+        if self.feasibility_adjacency and self.dset.open_world:
+            for idx, p in enumerate(self.pairs):
+                self.feasibility_scores[p] = 1. if p in self.dset.train_pairs else 0.
+                self.feasibility_margins[idx] =  0. if p in self.dset.train_pairs else -10.
+        else:
+            for idx, p in enumerate(self.pairs):
+                self.feasibility_scores[p] = 1.
+                self.feasibility_margins[idx] =  0.
 
 
     def init_embeddings(self, all_words):
@@ -89,7 +137,6 @@ class GraphFull(nn.Module):
                 composed_embed = (attr_embed + obj_embed) / 2
                 composition_embeds.append(composed_embed)
             composition_embeds = torch.stack(composition_embeds)
-            print('Compositional Embeddings are ', composition_embeds.shape)
             return composition_embeds
 
         # init with word embeddings
@@ -101,32 +148,38 @@ class GraphFull(nn.Module):
         return full_embeddings
 
 
-    def update_dict(self, wdict, row,col,data):
+    def update_adj(self, epochs=0.):
+        self.compute_feasibility(epochs)
+        adj = self.adj_from_pairs()
+        self.gcn.update_adj(adj)
+
+    def update_dict(self, wdict, row, col, data):
         wdict['row'].append(row)
         wdict['col'].append(col)
         wdict['data'].append(data)
 
+
     def adj_from_pairs(self):
 
         def edges_from_pairs(pairs):
-            weight_dict = {'data':[],'row':[],'col':[]}
-
+            weight_dict = {'data': [], 'row': [], 'col': []}
 
             for i in range(self.displacement):
-                self.update_dict(weight_dict,i,i,1.)
+                self.update_dict(weight_dict, i, i, 1.)
 
             for idx, (attr, obj) in enumerate(pairs):
                 attr_idx, obj_idx = self.attr_to_idx[attr], self.obj_to_idx[obj] + self.num_attrs
 
-                self.update_dict(weight_dict, attr_idx, obj_idx, 1.)
-                self.update_dict(weight_dict, obj_idx, attr_idx, 1.)
+                weight = self.feasibility_scores[(attr, obj)] if self.feasibility_adjacency else 1.
+
+                self.update_dict(weight_dict, attr_idx, obj_idx, weight)
+                self.update_dict(weight_dict, obj_idx, attr_idx, weight)
 
                 node_id = idx + self.displacement
-                self.update_dict(weight_dict,node_id,node_id,1.)
+                self.update_dict(weight_dict, node_id, node_id, 1.)
 
-                self.update_dict(weight_dict, node_id, attr_idx, 1.)
-                self.update_dict(weight_dict, node_id, obj_idx, 1.)
-
+                self.update_dict(weight_dict, node_id, attr_idx, weight)
+                self.update_dict(weight_dict, node_id, obj_idx, weight)
 
                 self.update_dict(weight_dict, attr_idx, node_id, 1.)
                 self.update_dict(weight_dict, obj_idx, node_id, 1.)
@@ -135,76 +188,167 @@ class GraphFull(nn.Module):
 
         edges = edges_from_pairs(self.pairs)
         adj = sp.csr_matrix((edges['data'], (edges['row'], edges['col'])),
-                            shape=(len(self.pairs)+self.displacement, len(self.pairs)+self.displacement))
+                            shape=(len(self.pairs) + self.displacement, len(self.pairs) + self.displacement))
 
         return adj
 
+    def get_pair_scores_objs(self, attr, obj, obj_embedding_sim):
+        score = -1.
+        for o in self.objs:
+            if o != obj and attr in self.attrs_by_obj_train[o]:
+                temp_score = obj_embedding_sim[(obj, o)]
+                if temp_score > score:
+                    score = temp_score
+        return score
 
+    def get_pair_scores_attrs(self, attr, obj, attr_embedding_sim):
+        score = -1.
+        for a in self.attrs:
+            if a != attr and obj in self.obj_by_attrs_train[a]:
+                temp_score = attr_embedding_sim[(attr, a)]
+                if temp_score > score:
+                    score = temp_score
+        return score
+
+
+    def compute_feasibility(self,epoch):
+        self.gcn.eval()
+        embeddings = self.gcn(self.embeddings).detach()
+
+        if self.training:
+            self.gcn.train()
+        obj_embeddings = embeddings[len(self.attrs):self.displacement]
+        obj_embedding_sim = compute_cosine_similarity(self.objs, obj_embeddings,
+                                                      return_dict=True)
+        attr_embeddings = embeddings[:len(self.attrs)]
+        attr_embedding_sim = compute_cosine_similarity(self.attrs, attr_embeddings,
+                                                       return_dict=True)
+
+        for (a,o) in self.pairs:
+                idx = self.pair2idx[(a, o)]
+                if (a, o) not in self.known_pairs:
+                    score_obj = self.get_pair_scores_objs(a, o, obj_embedding_sim)
+                    score_attr = self.get_pair_scores_attrs(a, o, attr_embedding_sim)
+                    score = (score_obj + score_attr) / 2
+                    self.feasibility_scores[(a,o)] = max(0.,score)
+                    self.feasibility_margins[idx] = score
+                else:
+                    self.feasibility_scores[(a,o)] = 1.
+                    self.feasibility_margins[idx] = 0.
+        self.feasibility_margins *= min(1., epoch / self.epoch_max_margin)*self.cosine_margin_factor
 
     def train_forward_normal(self, x):
         img, attrs, objs, pairs = x[0], x[1], x[2], x[3]
 
         if self.args.nlayers:
             img_feats = self.image_embedder(img)
+            
+            if self.args.lema:
+                attr_feats = self.img2attr(img)
+                obj_feats = self.img2obj(img)
         else:
             img_feats = (img)
 
+        if self.cosloss:
+            img_feats = F.normalize(img_feats, dim=1)
+            
+            if self.args.lema:
+                attr_feats = F.normalize(attr_feats, dim=1)
+                obj_feats = F.normalize(obj_feats, dim=1)
+
         current_embeddings = self.gcn(self.embeddings)
-       
+
         if self.args.train_only:
             pair_embed = current_embeddings[self.train_idx]
         else:
-            pair_embed = current_embeddings[self.num_attrs+self.num_objs:self.num_attrs+self.num_objs+self.num_pairs,:]
+            pair_embed = current_embeddings[
+                         self.num_attrs + self.num_objs:self.num_attrs + self.num_objs + self.num_pairs, :]
 
-        pair_embed = pair_embed.permute(1,0)
-        pair_pred = torch.matmul(img_feats, pair_embed) # cosine similarity
+        pair_embed = pair_embed.permute(1, 0)
+        pair_pred = torch.matmul(img_feats, pair_embed)
+
+        if self.args.lema:
+            attr_embs = current_embeddings[:self.num_attrs]
+            obj_embs = current_embeddings[self.num_attrs:self.num_attrs+self.num_objs]
+
+
+        if self.cosloss:
+            if self.dset.open_world:
+                pair_pred = (pair_pred + self.feasibility_margins) * self.scale
+            else:
+                pair_pred = pair_pred * self.scale
+
         loss = F.cross_entropy(pair_pred, pairs)
 
-        return  loss, None
+        if self.args.lema:
+            attr_pred = torch.matmul(attr_feats, attr_embs.T) * 0.5
+            obj_pred = torch.matmul(obj_feats, obj_embs.T) * 0.5
+
+            attr_pos_score = attr_pred[torch.arange(len(attrs)), attrs[torch.arange(len(attrs))]]
+            obj_pos_score = obj_pred[torch.arange(len(objs)), objs[torch.arange(len(objs))]]
+
+            attrs_list = [[x] for x in attrs.tolist()]
+            objs_list = [[x] for x in objs.tolist()]
+            attr_negs_score = torch.stack([attr_pred[idx][torch.tensor(list(set(range(attr_pred.size(1))) - set(exclude)))] for idx, exclude in enumerate(attrs_list)])
+            obj_negs_score = torch.stack([obj_pred[idx][torch.tensor(list(set(range(obj_pred.size(1))) - set(exclude)))] for idx, exclude in enumerate(objs_list)])
+
+            attr_lema_loss = torch.mean(attr_negs_score.sum(dim=1)) - torch.mean(attr_pos_score)
+            obj_lema_loss = torch.mean(obj_negs_score.sum(dim=1)) - torch.mean(obj_pos_score)
+
+            lema_loss = (attr_lema_loss + obj_lema_loss) * 0.3
+            loss += lema_loss
+
+        return loss, None
 
     def val_forward_dotpr(self, x):
-        img = x[0]
+        img, attrs, objs, pairs = x[0], x[1], x[2], x[3]
 
         if self.args.nlayers:
             img_feats = self.image_embedder(img)
+            if self.args.lema:
+                obj_feats = self.img2obj(img)
+                attr_feats = self.img2attr(img)
         else:
-            img_feats = (img)
+            img_feats = (img)  
 
-        current_embedddings = self.gcn(self.embeddings)
+        if self.cosloss:
+            img_feats = F.normalize(img_feats, dim=1)
+            if self.args.lema:
+                attr_feats = F.normalize(attr_feats, dim=1)
+                obj_feats = F.normalize(obj_feats, dim=1)
 
-        pair_embeds = current_embedddings[self.num_attrs+self.num_objs:self.num_attrs+self.num_objs+self.num_pairs,:].permute(1,0)
-
-        score = torch.matmul(img_feats, pair_embeds)
-
-        scores = {}
-        for itr, pair in enumerate(self.dset.pairs):
-            scores[pair] = score[:,self.dset.all_pair2idx[pair]]
-
-        return None, scores
-
-    def val_forward_distance_fast(self, x):
-        img = x[0]
-
-        img_feats = (self.image_embedder(img))
         current_embeddings = self.gcn(self.embeddings)
-        pair_embeds = current_embeddings[self.num_attrs+self.num_objs:,:]
 
-        batch_size, pairs, features = img_feats.shape[0], pair_embeds.shape[0], pair_embeds.shape[1]
-        img_feats = img_feats[:,None,:].expand(-1, pairs, -1)
-        pair_embeds = pair_embeds[None,:,:].expand(batch_size, -1, -1)
-        diff = (img_feats - pair_embeds)**2
-        score = diff.sum(2) * -1
+        pair_embeds = current_embeddings[
+                      self.num_attrs + self.num_objs:self.num_attrs + self.num_objs + self.num_pairs, :].permute(1, 0)
+        
+        pair_pred = torch.matmul(img_feats, pair_embeds)
+        score = pair_pred
+
+        if self.args.lema:
+            attr_embs = current_embeddings[:self.num_attrs]
+            obj_embs = current_embeddings[self.num_attrs:self.num_attrs+self.num_objs]
+
+            attr_pred = torch.matmul(attr_feats, attr_embs.T) * 0.5
+            obj_pred = torch.matmul(obj_feats, obj_embs.T) * 0.5
+        
+            lema_preds = (attr_pred.unsqueeze(2) + obj_pred.unsqueeze(1)).view(attr_pred.size(0),-1) 
+
+            score = pair_pred + torch.min(lema_preds, torch.tensor(0.))
 
         scores = {}
         for itr, pair in enumerate(self.dset.pairs):
-            scores[pair] = score[:,self.dset.all_pair2idx[pair]]
+            scores[pair] = score[:, self.dset.all_pair2idx[pair]]
 
-        return None, scores
+        return score, scores
+
 
     def forward(self, x):
         if self.training:
             loss, pred = self.train_forward(x)
+            return loss, pred
         else:
             with torch.no_grad():
-                loss, pred = self.val_forward(x)
-        return loss, pred
+                fast_pred, pred = self.val_forward(x)
+            return fast_pred, pred
+    
